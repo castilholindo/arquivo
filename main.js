4837c0af2ef9a86021b9ca7bcacfdca8ab15f951
castilholindo <104856525+castilholindo@users.noreply.github.com>
2024-03-22 11:00:29 -0300

    m

--- main.js
+++ main.js
@@ -24,4 +24,9 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
 
